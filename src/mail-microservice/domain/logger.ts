f9bc30f90c03192e161f0822ef79b6a58bce592e
FranAliss <franalissar@gmail.com>
2023-11-08 16:42:09 -0400

    Remove unnecessary context

--- src/mail-microservice/domain/logger.ts
+++ src/mail-microservice/domain/logger.ts
@@ -1,11 +1,4 @@
-interface MessageWithContext {
-  message: string;
-  context: Record<string, unknown>;
-}
-
-export type Message = string | MessageWithContext;
-
 export interface Logger {
-  info(message: Message): void;
-  error(message: Message): void;
+  info(message: string): void;
+  error(message: string): void;
 }
